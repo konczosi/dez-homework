8df26d26f1278533f9109e5bf12c4f11609dc3ea
konczosi <47603441+konczosi@users.noreply.github.com>
2023-02-15 11:25:05 +0100

    Update question_1.sql

--- week_3/question_1.sql
+++ week_3/question_1.sql
@@ -5,4 +5,9 @@ OPTIONS (
   uris = ['gs://dtc_data_lake_sage-nucleus-376020/data/fhv/fhv_tripdata_2019-*.csv.gz']
 );
 
+CREATE OR REPLACE TABLE `nytaxi.fhv_nonpartitioned_tripdata`
+AS SELECT * FROM `nytaxi.fhv_tripdata`;
+
 select count(1) from `nytaxi.fhv_tripdata`;
+
+select count(1) from `nytaxi.fhv_nonpartitioned_tripdata`;
